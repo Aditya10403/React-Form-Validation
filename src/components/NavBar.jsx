b8c4f1819790bd45eb14ffd29a09522e342ad0ed
Aditya10403 <adityashukla@gmail.com>
2024-05-28 12:13:06 +0530

    update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div>
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://adityashukla-portfolio.vercel.app/" className="flex items-center rtl:space-x-reverse">
             <img src="src/assets/AS.png" className="h-8" alt="Logo" />
